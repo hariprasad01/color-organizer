2837cae330ff9d370ac95f31416453fc4bf50543
Prasad <hari.prasad@nokia.com>
2022-01-10 23:04:00 +0530

    Adding useMemo logic

--- src/components/AddColorForm.js
+++ src/components/AddColorForm.js
@@ -1,16 +1,24 @@
-import React, { useRef, useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import "../App.css";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { ColorPicker, createColor } from 'material-ui-color';
 import { useInput } from "../customHooks/input-hook";
 import { useColor } from "../customHooks/colors-hook";
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
 
 export default function AddColorForm() {
     const txtTitle = useRef();
     const [color, setColor] = useState(createColor("#000"));
     const [rating, resetRating] = useInput(0);
     const { addColor } = useColor();
+    const [checked, setChecked] = useState(false);
+
+    const handleChange = () => {
+        setChecked(!checked)
+    }
 
     const submit = e => {
         e.preventDefault();
@@ -21,17 +29,40 @@ export default function AddColorForm() {
         setColor(createColor("#000"));
     }
 
+    //useMemo will cache the respose and if the data in the dependency array is the same then the cached value will be returned
+    const compliment = useMemo(() => {
+        if (rating.value > 0) {
+            let x = 1
+            while (x < 4000000000) {
+                x++
+            }
+            if (rating.value < 3) {
+                return <h3>This is a bad color</h3>
+            } else {
+                return <h3>This is a great color</h3>
+            }
+        }
+        return undefined;
+    }, [rating.value])
+
+
     return (
         <form className="m-8" style={{ display: "flex" }} onSubmit={submit}>
-            <TextField style={{ margin: 8 }} inputProps={{ maxLength: "20" }} inputRef={txtTitle} type="text" placeholder="Title" variant="standard" label="Color title" required ></TextField>
-            <TextField style={{ margin: 8 }} inputProps={{ max: 5, min: 0 }} type="number" placeholder="Rating" variant="standard" label="Rating" required {...rating}></TextField>
+            <TextField style={{ margin: 8 }} disabled={checked} inputProps={{ maxLength: "20" }} defaultValue="Test" inputRef={txtTitle} type="text" placeholder="Title" variant="standard" label="Color title" required ></TextField>
+            <TextField style={{ margin: 8 }} disabled={checked} inputProps={{ max: 5, min: 0 }} type="number" placeholder="Rating" variant="standard" label="Rating" required {...rating}></TextField>
             <div style={{ margin: "inherit", marginTop: "auto" }}>
                 <ColorPicker required value={color} onChange={(event) => {
-                    console.log('event: ', event);
+                    // console.log('event: ', event);
                     setColor(event)
                 }}></ColorPicker>
             </div>
-            <Button style={{ margin: 8 }} variant="contained" type="submit">ADD</Button>
+            <Button style={{ margin: 8 }} variant="contained" disabled={checked} type="submit">ADD</Button>
+            <FormControlLabel style={{ marginLeft: 250 }} control={
+                <Switch checked={checked}
+                    onChange={handleChange}
+                    inputProps={{ 'aria-label': 'controlled' }} />}
+                label="Disable input" />
+            {compliment}
         </form>
     )
 }
