80c8367de7ff23ca5f5273f6ae6530ac7db199a5
hariprasad01 <prasadhari905@gmail.com>
2022-02-19 00:52:48 +0530

    Fixing image import

--- src/components/NoColor.js
+++ src/components/NoColor.js
@@ -1,10 +1,11 @@
 import React from "react";
+import emptyImage from '../data/empty.png';
 
 export default function NoColor() {
     const altText = "Nothing here, add some colors"
     return (
         <div style={{ alignContent: "center" }}>
-            <img src={require("file:/C:/Users/haprasad/Documents/React/create-react-app/color-organizer/src/data/empty.png")} alt={altText} height="50%" width="50%"></img>
+            <img src={emptyImage} alt={altText} height="50%" width="50%"></img>
         </div>
     )
 }
